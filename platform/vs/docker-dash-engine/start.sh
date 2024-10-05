56e40e6bb40723dd5eb25182194c83ee34b12b44
r12f <r12f.code@gmail.com>
2024-10-05 05:23:42 +0000

    Update dash engine start command,

--- platform/vs/docker-dash-engine/start.sh
+++ platform/vs/docker-dash-engine/start.sh
@@ -1,19 +1,3 @@
 #!/usr/bin/env bash
 
-function rename_intf() {
-    if ! sudo ifconfig $1 > /dev/null 2&>1; then
-        if ! sudo ifconfig $2 > /dev/null 2&>1; then
-            echo "Interface $1 is not found. BMv2 data plane will not work."
-            exit 1
-        fi
-    else
-        sudo ip link set dev $1 down
-        sudo ip link set dev $1 name $2
-        sudo ip link set dev $2 up
-    fi
-}
-
-rename_intf eth1 vdp0
-rename_intf eth2 vdp1
-
-simple_switch_grpc --interface 0@vdp0 --interface 1@vdp1 --log-console --no-p4
+simple_switch_grpc --interface 0@vdp0 --interface 1@vdp1 --interface 2@Ethernet0 --interface 3@Ethernet4 --log-console --no-p4
