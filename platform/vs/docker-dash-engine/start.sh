52f1689dca11b827fe09482a6c4534c760078ec5
r12f <r12f.code@gmail.com>
2024-10-06 02:11:02 +0000

    Make p4runtime shell working in dash-engine.

--- platform/vs/docker-dash-engine/start.sh
+++ platform/vs/docker-dash-engine/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-simple_switch_grpc --interface 0@vdp0 --interface 1@vdp1 --interface 2@Ethernet0 --interface 3@Ethernet4 --log-console --no-p4
+simple_switch_grpc --interface 0@eth1 --interface 1@Ethernet0 --interface 2@eth2 --interface 3@Ethernet4 --log-console --no-p4
