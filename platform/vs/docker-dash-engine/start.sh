a7fc0c618c5be9cc35c3878272a62ff4a640894f
r12f <r12f.code@gmail.com>
2024-10-09 06:06:44 +0000

    Don't rename eth1, 2.

--- platform/vs/docker-dash-engine/start.sh
+++ platform/vs/docker-dash-engine/start.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-simple_switch_grpc --interface 0@eth1 --interface 1@Ethernet0 --interface 2@eth2 --interface 3@Ethernet4 --log-console --no-p4
+# simple_switch_grpc --interface 0@eth1 --interface 1@Ethernet0 --interface 2@eth2 --interface 3@Ethernet4 --log-console --no-p4
+simple_switch_grpc --interface 0@eth1 --interface 1@eth2 --log-console --no-p4
